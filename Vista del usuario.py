7d851b122c417e58063f7b42e2367abda4904d75
Jefferson O. <jefferson240111@gmail.com>
2024-08-23 08:22:01 -0500

    cambio a las vistas V2
    se eliminaron 2 vistas innecesarias

--- Vista del usuario.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-class vistadeinicio(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        self.titulo = tk.Label(self, text="Inicio de Sesión")
-        self.titulo.pack()
-        # Aquí se agregan widgets para el usuario y la contraseña.
