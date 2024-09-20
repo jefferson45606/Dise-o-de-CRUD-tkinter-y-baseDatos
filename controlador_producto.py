6e6878d6890c8a35970f3122a431f902a8a4bb98
Jefferson O. <jefferson240111@gmail.com>
2024-09-19 21:56:17 -0500

    a

--- controlador_producto.py
+++ controlador_producto.py
@@ -1,31 +1,17 @@
-from modelo_producto import Producto
-
 class ControladorProducto:
     def __init__(self):
-        self.catalogo = []  # Aquí guardaremos los productos de manera temporal
+        self.db_manager = DBManager()
 
-    def agregar_producto(self, nombre, descripcion, precio, stock):
-        id_producto = len(self.catalogo) + 1  # Generar un ID de producto simple
-        nuevo_producto = Producto(id_producto, nombre, descripcion, precio, stock)
-        self.catalogo.append(nuevo_producto)
+    def registrar_producto(self, nombre, descripcion, precio, cantidad):
+        consulta = "INSERT INTO productos (nombre, descripcion, precio, cantidad) VALUES (%s, %s, %s, %s)"
+        return self.db_manager.ejecutar_consulta(consulta, (nombre, descripcion, precio, cantidad))
 
-    def obtener_productos(self):
-        return self.catalogo
+    def obtener_todos_los_productos(self):
+        """Obtener todos los productos de la base de datos"""
+        consulta = "SELECT * FROM productos"
+        return self.db_manager.fetch_all(consulta)
 
-    def generar_informe_productos(self):
-        try:
-            # Crear un archivo de texto llamado "informe_productos.txt"
-            with open("informe_productos.txt", "w") as archivo:
-                archivo.write("Informe de Productos\n")
-                archivo.write("=" * 50 + "\n")
-                archivo.write(f"{'ID':<5} {'Nombre':<15} {'Descripción':<25} {'Precio':<10} {'Stock':<5}\n")
-                archivo.write("-" * 50 + "\n")
-
-                # Escribir los detalles de cada producto
-                for producto in self.catalogo:
-                    archivo.write(f"{producto.id_producto:<5} {producto.nombre:<15} {producto.descripcion:<25} {producto.precio:<10} {producto.stock:<5}\n")
-
-            return True  # Si se genera el archivo correctamente
-        except Exception as e:
-            print(f"Error al generar el informe: {e}")
-            return False  # Si ocurre un error
+    def buscar_producto_por_nombre(self, nombre):
+        """Buscar productos en la base de datos por nombre"""
+        consulta = "SELECT * FROM productos WHERE nombre LIKE %s"
+        return self.db_manager.fetch_all(consulta, (f"%{nombre}%",))
