a332ec0b253816fcffac2276e5cdd02747a9c4d5
Jefferson O. <jefferson240111@gmail.com>
2024-09-26 23:08:31 -0500

    bd V2
    se actualizo la base de datos, se logro conectar

--- bd/bd.py
+++ bd/bd.py
@@ -7,8 +7,8 @@ def conectar_db():
     try:
         conn = mysql.connector.connect(
             host='localhost',
-            user='jefferson',       
-            password='2005', 
+            user='anderson',       
+            password='12345', 
             database='catalogo_avenas'          
         )
         if conn.is_connected():
@@ -21,9 +21,9 @@ def conectar_db():
 def crear_tablas(cursor):
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS usuario (
-            ID_usuario BIGINT PRIMARY KEY,
+            ID_usuario BIGINT PRIMARY KEY AUTO_INCREMENT,
             Usuario VARCHAR(255) NOT NULL,
-            Contraseña INT(50) NOT NULL,
+            Contraseña VARCHAR(255) NOT NULL,
             Rol VARCHAR(255) NOT NULL
         );
     ''')
@@ -40,14 +40,14 @@ def crear_tablas(cursor):
     ''')
     print("Tabla 'producto' creada o ya existe.")
 
-def agregar_usuario(cursor, username, password):
-    cursor.execute('INSERT INTO usuarios (username, password) VALUES (%s, %s)', (username, password))
+def agregar_usuario(cursor, username, password, rol):
+    cursor.execute('INSERT INTO usuario (Usuario, Contraseña, Rol) VALUES (%s, %s, %s)', (username, password, rol))
 
 def verificar_usuario(username, password):
     conn = conectar_db()
     if conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM usuarios WHERE username = %s AND password = %s', (username, password))
+        cursor.execute('SELECT * FROM usuario WHERE Usuario = %s AND Contraseña = %s', (username, password))
         user = cursor.fetchone()
         cursor.close()
         conn.close()
@@ -58,12 +58,13 @@ def verificar_usuario(username, password):
 def registrar_usuario_gui():
     username = entry_usuario_nombre.get()
     password = entry_usuario_password.get()
-    if username and password:
+    rol = entry_usuario_rol.get()
+    if username and password and rol:
         conn = conectar_db()
         if conn:
             cursor = conn.cursor()
             try:
-                agregar_usuario(cursor, username, password)
+                agregar_usuario(cursor, username, password, rol)
                 conn.commit()
                 messagebox.showinfo("Éxito", "Usuario registrado.")
                 entry_usuario_nombre.delete(0, tk.END)
@@ -88,70 +89,71 @@ def iniciar_sesion_gui():
 
 def mostrar_ventana_principal(username):
     ventana_principal = tk.Tk()
-    ventana_principal.title("Inventario de catalogo de avenas")
+    ventana_principal.title("Inventario de catálogo de avenas")
 
     label_bienvenida = tk.Label(ventana_principal, text=f"Bienvenido(a), {username}!")
     label_bienvenida.pack(pady=10)
 
-    # Conectar a la base de datos
     conn = conectar_db()
     if conn is not None:
         cursor = conn.cursor()
         crear_tablas(cursor)
 
-        # Sección para Pizzas
+        # registro de avenas
         frame_avenas = tk.Frame(ventana_principal)
         frame_avenas.pack(pady=10)
 
-        label_avena_nombre = tk.Label(frame_avenas, text="Nombre de de la avena:")
+        label_avena_nombre = tk.Label(frame_avenas, text="Nombre del producto:")
         label_avena_nombre.grid(row=0, column=0)
 
         entry_avena_nombre = tk.Entry(frame_avenas)
         entry_avena_nombre.grid(row=0, column=1)
 
-        label_avena_tamaño = tk.Label(frame_avenas, text="Descripcion del producto:")
-        label_avena_tamaño.grid(row=1, column=0)
+        label_avena_descripcion = tk.Label(frame_avenas, text="Descripción del producto:")
+        label_avena_descripcion.grid(row=1, column=0)
 
-        entry_avena_tamaño = tk.Entry(frame_avenas)
-        entry_avena_tamaño.grid(row=1, column=1)
-        
-        label_avena_tamaño = tk.Label(frame_avenas, text="Precio del producto:")
-        label_avena_tamaño.grid(row=2, column=0)
-        
-        entry_avena_tamaño = tk.Entry(frame_avenas)
-        entry_avena_tamaño.grid(row=2, column=1)
-        
-        label_avena_tamaño = tk.Label(frame_avenas, text="Cantidad en stock:")
-        label_avena_tamaño.grid(row=3, column=0)
-        
-        entry_avena_tamaño = tk.Entry(frame_avenas)
-        entry_avena_tamaño.grid(row=3, column=1)
+        entry_avena_descripcion = tk.Entry(frame_avenas)
+        entry_avena_descripcion.grid(row=1, column=1)
 
-        button_agregar_avena = tk.Button(frame_avenas, text="Agregar avena", command=lambda: agregar_avena(cursor, entry_avena_nombre.get(), entry_avena_tamaño.get()))
-        button_agregar_avena.grid(row=2, columnspan=2)
+        label_avena_precio = tk.Label(frame_avenas, text="Precio del producto:")
+        label_avena_precio.grid(row=2, column=0)
 
+        entry_avena_precio = tk.Entry(frame_avenas)
+        entry_avena_precio.grid(row=2, column=1)
 
-def agregar_avena(cursor, Nombre, Descripcion, Precio, Cantidad_Stock):
-    if Nombre and Descripcion and Precio and Cantidad_Stock:
+        label_avena_stock = tk.Label(frame_avenas, text="Cantidad en stock:")
+        label_avena_stock.grid(row=3, column=0)
+
+        entry_avena_stock = tk.Entry(frame_avenas)
+        entry_avena_stock.grid(row=3, column=1)
+
+        button_agregar_avena = tk.Button(frame_avenas, text="Agregar avena", command=lambda: agregar_avena(cursor, entry_avena_nombre.get(), entry_avena_descripcion.get(), entry_avena_precio.get(), entry_avena_stock.get()))
+        button_agregar_avena.grid(row=4, columnspan=2)
+        
+        button_consultar_avenas = tk.Button(ventana_principal, text="Consultar Avenas", command=lambda: consultar_avenas(cursor))
+        button_consultar_avenas.pack(pady=5)
+
+def agregar_avena(cursor, nombre, descripcion, precio, stock):
+    if nombre and descripcion and precio and stock:
         conn = conectar_db()
         if conn:
             cursor = conn.cursor()
-            cursor.execute('INSERT INTO pizza (Nombre, Descripcion, Precio, Cantidad_Stock) VALUES (%s, %s)', (Nombre, Descripcion, Precio, Cantidad_Stock))
+            cursor.execute('INSERT INTO producto (Nombre, Descripcion, Precio, Cantidad_Stock) VALUES (%s, %s, %s, %s)', (nombre, descripcion, precio, stock))
             conn.commit()
-            messagebox.showinfo("Éxito", f"avena '{Nombre}' agregada.")
+            messagebox.showinfo("Éxito", f"Avena '{nombre}' agregada.")
             cursor.close()
             conn.close()
     else:
         messagebox.showwarning("Advertencia", "Por favor, completa todos los campos.")
-
+        
 def consultar_avenas(cursor):
     conn = conectar_db()
     if conn:
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM producto')
         resultados = cursor.fetchall()
-        avenas_texto = "\n".join([f"ID_preducto: {fila[0]}, Nombre: {fila[1]}, Descripcion: {fila[2]}, Precio: {fila[3]}, Cantidad_Stock: {fila[4]}" for fila in resultados])
-        messagebox.showinfo("Avenas en la Base de Datos", avenas_texto if avenas_texto else "No hay ninguna avena registrada.")
+        avenas_texto = "\n".join([f"ID_producto: {fila[0]}, Nombre: {fila[1]}, Descripcion: {fila[2]}, Precio: {fila[3]}, Stock: {fila[4]}" for fila in resultados])
+        messagebox.showinfo("Avenas en la Base de Datos", avenas_texto if avenas_texto else "No hay avenas registradas.")
         cursor.close()
         conn.close()
 
@@ -181,8 +183,13 @@ label_usuario_password.grid(row=1, column=0)
 entry_usuario_password = tk.Entry(frame_usuario, show='*')
 entry_usuario_password.grid(row=1, column=1)
 
+label_usuario_rol = tk.Label(frame_usuario, text="Rol:")
+label_usuario_rol.grid(row=2, column=0)
+entry_usuario_rol = tk.Entry(frame_usuario)
+entry_usuario_rol.grid(row=2, column=1)
+
 button_registrar_usuario = tk.Button(frame_usuario, text="Registrar Usuario", command=registrar_usuario_gui)
-button_registrar_usuario.grid(row=2, columnspan=2)
+button_registrar_usuario.grid(row=3, columnspan=2)
 
 # Sección de Inicio de Sesión
 frame_inicio_sesion = tk.Frame(ventana_inicio)
@@ -199,6 +206,6 @@ entry_inicio_password = tk.Entry(frame_inicio_sesion, show='*')
 entry_inicio_password.grid(row=1, column=1)
 
 button_iniciar_sesion = tk.Button(frame_inicio_sesion, text="Iniciar Sesión", command=iniciar_sesion_gui)
-button_iniciar_sesion.grid(row=2, columnspan=2)
+button_iniciar_sesion.grid(row=3, columnspan=2)
 
 ventana_inicio.mainloop()
